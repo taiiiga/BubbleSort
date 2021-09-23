9d0eeaa92a32cba249e10418649063dc0c7f4061
”Iskander <iskanderkhisamov@gmail.com>
2021-09-23 20:02:30 +0300

    Initial commit

---
+++ Program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TopLibrary;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {800,11,50,771,649,770,240, 9};
+            var output = Sorter.sort(arr);
+            foreach (var t in output)
+            {
+                Console.WriteLine(t);
+            }
+        }
+    }
+}
